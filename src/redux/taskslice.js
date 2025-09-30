cb1156e5429944c85bcf3b09b499000c6d6a6d9d
codewith-isha <codeoncanvas20@gmail.com>
2025-09-30 17:10:31 +0530

    added localstorage

--- src/redux/taskslice.js
+++ src/redux/taskslice.js
@@ -1,21 +1,36 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+const loadlocalstorage = ()=>{
+  const saved = localStorage.getItem("tasks")
+  return saved? JSON.parse(saved):[]
+}
+
+const saveLocalstorage = (tasks)=>{
+  localStorage.setItem("tasks" , JSON.stringify(tasks))
+}
+
+
 const initialState = {
   tasks:[],
 }
 const taskSlice = createSlice({
   name:"tasks",
-  initialState,
+  initialState:{
+    tasks:loadlocalstorage()
+  },
   reducers:{
     addTask:(state,action)=>{
       state.tasks.push(action.payload)
+      saveLocalstorage(state.tasks)
     },
     deleteTask:(state,action)=>{
       state.tasks = state.tasks.filter((_,i)=> i !== action.payload)
+      saveLocalstorage(state.tasks)
     },
     updateTask:(state,action) =>{
       const {index , newTask} = action.payload;
       state.tasks[index] = newTask
+      saveLocalstorage(state.tasks)
     }
   }
 })
